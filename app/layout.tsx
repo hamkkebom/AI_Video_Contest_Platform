4c3438f061299a216f90a82ba1f6d6e91db5bdf9
hamkkebom12 <255367038+hamkkebom@users.noreply.github.com>
2026-02-25 21:32:51 +0900

    feat: Google Search Console 도메인 인증 메타 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌳</text></svg>",
   },
+  verification: {
+    google: "GmdzYkOhfANE8IQ3JXMd_BOqtXcAuykFU1CjDd3Wa1M",
+  },
 };
 
 type RootLayoutProps = {
